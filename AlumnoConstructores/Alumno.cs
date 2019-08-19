fed1b7648efd0e5ae15a5805674506631cd5a2c4
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-19 12:39:34 -0700

    Agregar archivos de proyecto.

---
+++ AlumnoConstructores/Alumno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlumnoConstructores
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        //public DateTime Fechadenacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Alumno()
+        {
+            Nombre = "Sin asignar";
+            Matricula = "Sin asignar";
+            /*fechadenacimiento = ;*/
+            Direccion = "Sin asignar";
+            Telefono = "Sin asignar";
+        }
+
+        public Alumno(string nombre, string matricula /*DateTime fechadenacimiento, string direccion, string telefono*/)
+        {
+            Nombre = nombre;
+            Matricula = matricula;
+            /*fechadenacimiento
+            Direccion = "Sin asignar";
+            Telefono = "Sin asignar";*/
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ AlumnoConstructores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlumnoConstructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.Nombre = "Pendiente";
+            alumno1.Matricula = "Sin asignar";
+
+            Console.WriteLine(
+                "Nombre: " + alumno1.Nombre);
+            Console.WriteLine(
+                "Matricula: " + alumno1.Matricula);
+
+
+            Alumno alumno2 =
+                new Alumno("Pendiente", "sin asignar");
+            Console.WriteLine("alumno2");
+            Console.WriteLine(
+                "Nombre: " + alumno2.Nombre);
+            Console.WriteLine(
+                "Matricula: " + alumno2.Matricula);
+
+
+            Console.Read();
+        }
+    }
+}
